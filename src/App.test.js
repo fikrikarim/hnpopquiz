e0d66ecd0362993c7cfc4269e9062956134819a3
Fikri Karim <fkfikrikarim@gmail.com>
2018-12-02 11:50:08 +0700

    Add travis configuration

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "match-media";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
