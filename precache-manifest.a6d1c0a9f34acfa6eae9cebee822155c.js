8347d074596802fbb79864f4ae762b9cebb20408
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-02 13:49:25 +0000

    Deploy fikrikarim/hnpopquiz to github.com/fikrikarim/hnpopquiz.git:gh-pages

---
+++ precache-manifest.a6d1c0a9f34acfa6eae9cebee822155c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a33638b372aa5aa1949",
+    "url": "/hnpopquiz/static/css/main.ada4649e.chunk.css"
+  },
+  {
+    "revision": "8a33638b372aa5aa1949",
+    "url": "/hnpopquiz/static/js/main.8a33638b.chunk.js"
+  },
+  {
+    "revision": "8f72837d159097eb065b",
+    "url": "/hnpopquiz/static/css/1.d5fed480.chunk.css"
+  },
+  {
+    "revision": "8f72837d159097eb065b",
+    "url": "/hnpopquiz/static/js/1.8f72837d.chunk.js"
+  },
+  {
+    "revision": "68a5bd960c73466994c4",
+    "url": "/hnpopquiz/static/js/runtime~main.68a5bd96.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/hnpopquiz/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/hnpopquiz/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/hnpopquiz/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/hnpopquiz/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "1b74edf16057938dadefcd55dd1f1228",
+    "url": "/hnpopquiz/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/hnpopquiz/precache-manifest.a6d1c0a9f34acfa6eae9cebee822155c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/hnpopquiz/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
