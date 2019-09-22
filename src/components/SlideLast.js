f765838c0dea455efd947928a14b4e6e991f60b9
Fikri Karim <fkfikrikarim@gmail.com>
2019-09-22 15:50:07 +0700

    Add Github Fork button to SlideLast

--- src/components/SlideLast.js
+++ src/components/SlideLast.js
@@ -3,11 +3,20 @@ import React from "react";
 export default function SlideLast() {
   return (
     <div className="slide slide-last">
-      <h2>Share this quiz</h2>
-      <h2>Have better questions?</h2>
-      <button>Fork the repo</button>
-      <p>or</p>
-      <button>Open a Pull Request</button>
+      <h2>Consider to share this quiz if you like it</h2>
+
+      <br />
+
+      <h3>Have better questions?</h3>
+
+      <iframe
+        title="Github Fork Button"
+        src="https://ghbtns.com/github-btn.html?user=fikrikarim&repo=hnpopquiz&type=fork&count=true&size=large"
+        frameBorder="0"
+        scrolling="0"
+        width="158px"
+        height="30px"
+      />
     </div>
   );
 }
