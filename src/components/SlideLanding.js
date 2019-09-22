f2a2e0e5d85087b4ddc5f166c116f695dadad1e8
Fikri Karim <fkfikrikarim@gmail.com>
2019-09-22 16:37:59 +0700

    Update the case for the title

--- src/components/SlideLanding.js
+++ src/components/SlideLanding.js
@@ -6,7 +6,7 @@ export default function SlideLanding(props) {
   return (
     <div className="slide slide-landing">
       <h1>HN Pop Quiz</h1>
-      <h2>How much useless things do you know about HN?</h2>
+      <h2>How much useless things do you know about HackerNews?</h2>
 
       <button className="pure-button pure-button-primary" onClick={onClick}>
         Let's go
